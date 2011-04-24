c474ca8dbab9ff040786437697920aa14ba9e29e
Marcello Bastea-Forte <marcello@marcello-dev.local>
2011-04-24 14:38:35 -0400

    initial commit!

---
+++ Readme.md
@@ -0,0 +1,55 @@
+node-tosource
+===========
+Super simple function to convert JavaScript objects back to source code.
+
+See [test.js][1] for example usage.
+
+Examples
+--------
+
+The following code:
+    var toSource = require('tosource')
+    console.log(toSource(
+        [ 4, 5, 6, "hello", {
+            a:2,
+            'b':3,
+            '1':4,
+            'if':5,
+            'undefined':undefined,
+            'null':null,
+            foo: function(bar) {
+                console.log("woo! a is "+a)
+                console.log("and bar is "+bar)
+            }
+        },
+        /we$/gi,
+        new Date("Wed, 09 Aug 1995 00:00:00 GMT")]
+    ))
+
+
+Outputs:
+    [ 4,
+      5,
+      6,
+      "hello",
+      { "1":4,
+        a:2,
+        b:3,
+        "if":5,
+        "undefined":undefined,
+        "null":null,
+        foo:function (bar) {
+                console.log("woo! a is "+a)
+                console.log("and bar is "+bar)
+            } },
+      /we$/gi,
+      new Date(807926400000) ]
+
+
+
+License
+-------
+Waiter is open source software under the [zlib license][2].
+
+[1]: https://github.com/marcello3d/node-tosource/blob/master/test.js
+[2]: https://github.com/marcello3d/node-tosource/blob/master/LICENSE
---
+++ tosource.js
@@ -0,0 +1,48 @@
+/* toSource by Marcello Bastea-Forte - zlib license */
+module.exports = function(object, filter, indent, startingIndent) {
+    var seen = []
+    return walk(object, filter, indent === undefined ? '  ' : (indent || ''), startingIndent || '')
+
+    function walk(object, filter, indent, currentIndent) {
+        var nextIndent = currentIndent + indent
+        object = filter ? filter(object) : object
+        if (object === null) return 'null'
+
+        if (object instanceof RegExp) return object.toString()
+        if (object instanceof Date) return 'new Date('+object.getTime()+')'
+
+        switch (typeof object) {
+            case 'undefined':
+                return 'undefined'
+            case 'string':
+            case 'number':
+            case 'boolean':
+                return JSON.stringify(object)
+            case 'function':
+                return object.toString()
+        }
+
+        if (seen.indexOf(object) >= 0) return '{$circularReference:1}'
+        seen.push(object)
+
+        function join(elements) {
+            return indent.slice(1) + elements.join(','+(indent&&'\n')+nextIndent) + (indent ? ' ' : '');
+        }
+
+        if (Array.isArray(object)) {
+            return '[' + join(object.map(function(element){
+                return walk(element, filter, indent, nextIndent)
+            })) + ']'
+        }
+        var keys = Object.keys(object)
+        return keys.length ? '{' + join(keys.map(function (key) {
+            return (legalKey(key) ? key : JSON.stringify(key)) + ':' + walk(object[key], filter, indent, nextIndent)
+        })) + '}' : '{}'
+    }
+}
+
+var KEYWORD_REGEXP = /^(abstract|boolean|break|byte|case|catch|char|class|const|continue|debugger|default|delete|do|double|else|enum|export|extends|false|final|finally|float|for|function|goto|if|implements|import|in|instanceof|int|interface|long|native|new|null|package|private|protected|public|return|short|static|super|switch|synchronized|this|throw|throws|transient|true|try|typeof|undefined|var|void|volatile|while|with)$/
+
+function legalKey(string) {
+    return /^[a-z_$][0-9a-z_$]*$/gi.test(string) && !KEYWORD_REGEXP.test(string)
+}
